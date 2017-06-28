4b19cd06a5b735c4f46377b8c90ea736ec59975a
Vignesh Sushrutha Raghavan <vignesh.raghavan@iit.it>
2017-06-28 10:46:05 +0200

    Changed optimization functor and problem solver options. Works well in simulation.

--- walkman_urdf_calibration/include/walkman_urdf_calibration/calibrate_urdf.h
+++ walkman_urdf_calibration/include/walkman_urdf_calibration/calibrate_urdf.h
@@ -21,6 +21,8 @@
 #include <tf_conversions/tf_eigen.h>
 #include "walkman_urdf_calibration/OptimizeUrdfJoints.h"
 
+int test_one_;
+
 struct JointToCalibrate{
   std::string joint_name_;
   std::string parent_name_;
@@ -34,16 +36,20 @@ struct MinimizeUrdfError{
   {
     joint_changes_ = joint_changes;
     number_of_joints_=number_of_joints;
+    test_one_=1;
   }
-  virtual ~MinimizeUrdfError() {}
-  Eigen::Matrix3f eulerToRot( float roll,float pitch,float yaw ) const
+  virtual ~MinimizeUrdfError()
   {
-    Eigen::AngleAxisf rollAngle(roll, Eigen::Vector3f::UnitX());
-    Eigen::AngleAxisf pitchAngle(pitch, Eigen::Vector3f::UnitY());
-    Eigen::AngleAxisf yawAngle(yaw, Eigen::Vector3f::UnitZ());
-
-    Eigen::Quaternionf q = rollAngle*pitchAngle*yawAngle;
-    return q.toRotationMatrix();
+    if(joint_changes_!=NULL)
+    delete[] joint_changes_;
+  }
+  Eigen::Matrix3f eulerToRot( float roll,float pitch,float yaw ) const //Obtained from Urdf::model::joint::pose
+  {
+    Eigen::Matrix3f rot;
+    rot= Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitX())
+  * Eigen::AngleAxisf(pitch,  Eigen::Vector3f::UnitY())
+  * Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ());
+  return rot;
   }
   Eigen::Matrix4f vectorToTransformMat(float px,float py, float pz, float roll, float pitch, float yaw) const
   {
@@ -52,18 +58,27 @@ struct MinimizeUrdfError{
     transform_matrix.block<3,3>(0,0) = eulerToRot(roll,pitch,yaw);
     transform_matrix(0,3) = px;
     transform_matrix(1,3) = py;
-    transform_matrix(2,3) = py;
+    transform_matrix(2,3) = pz;
+    transform_matrix(3,3) = 1.0;
     return transform_matrix;
   }
-  bool operator()(const double* const* joint_init_param, double* residual) const
+  bool operator()(const double * const  joint_init_param, double* residual) const
   {
     Eigen::Matrix4f new_calculated_pose = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f joint_init_matrix,joint_change_matrix;
     for(int i=0;i<number_of_joints_;i++)
     {
-        joint_init_matrix=vectorToTransformMat(float(*joint_init_param[(i+1)*6-6]),float(*joint_init_param[(i+1)*6-5]),float(*joint_init_param[(i+1)*6-4]),float(*joint_init_param[(i+1)*6-3]),float(*joint_init_param[(i+1)*6-2]),float(*joint_init_param[(i+1)*6-1]));
-        joint_change_matrix=vectorToTransformMat(joint_changes_[(i+1)*6-6],joint_changes_[(i+1)*6-5],joint_changes_[(i+1)*6-4],joint_changes_[(i+1)*6-3],joint_changes_[(i+1)*6-2],joint_changes_[(i+1)*6-1]);
-        new_calculated_pose*=joint_init_matrix*joint_change_matrix;
+      joint_init_matrix=vectorToTransformMat(float(joint_init_param[(i+1)*6-6]),float(joint_init_param[(i+1)*6-5]),float(joint_init_param[(i+1)*6-4]),float(joint_init_param[(i+1)*6-3]),float(joint_init_param[(i+1)*6-2]),float(joint_init_param[(i+1)*6-1]));
+      joint_change_matrix=vectorToTransformMat(joint_changes_[(i+1)*6-6],joint_changes_[(i+1)*6-5],joint_changes_[(i+1)*6-4],joint_changes_[(i+1)*6-3],joint_changes_[(i+1)*6-2],joint_changes_[(i+1)*6-1]);
+      new_calculated_pose=(joint_change_matrix*joint_init_matrix)*new_calculated_pose;
+    }
+    if(test_one_==1)
+    {
+      std::cout<<"\n Blah blah "<<number_of_joints_;
+      for(int k=0;k<60;k++)
+      std::cout<<"\n"<<joint_changes_[k];
+      test_one_++;
+      std::cout<<"\nEstimated \n"<<new_calculated_pose<<"\n actual\n"<<waist_hand_opti_track_;
     }
     Eigen::Vector3f new_euler_angles=new_calculated_pose.block<3,3>(0,0).eulerAngles(0,1,2);
     Eigen::Vector3f observed_euler_angles=waist_hand_opti_track_.block<3,3>(0,0).eulerAngles(0,1,2);
@@ -77,55 +92,57 @@ struct MinimizeUrdfError{
     return true;
   }
 
-  static ceres::CostFunction* Create(Eigen::Matrix4f waist_hand_opti_track,float* joint_changes, int number_of_joints)
+  /*static ceres::CostFunction* Create(Eigen::Matrix4f waist_hand_opti_track,float* joint_changes, int number_of_joints)
   {
-      ceres::DynamicNumericDiffCostFunction<MinimizeUrdfError> * func;
-      func = new ceres::DynamicNumericDiffCostFunction<MinimizeUrdfError>(new MinimizeUrdfError(waist_hand_opti_track,joint_changes, number_of_joints));
-      func->AddParameterBlock(number_of_joints);
-      func->SetNumResiduals(6);
-      return static_cast<ceres::CostFunction*>(func);
+  return( new ceres::AutoDiffCostFunction<MinimizeUrdfError,6,number_of_joints*6>(new MinimizeUrdfError(waist_hand_opti_track,joint_changes, number_of_joints)));
 
-  }
-  Eigen::Matrix4f waist_hand_opti_track_;
-  float* joint_changes_;
-  int number_of_joints_;
+
+}*/
+Eigen::Matrix4f waist_hand_opti_track_;
+float* joint_changes_;
+int number_of_joints_;
 };
-  class UrdfCalibrator
-  {
-  public:
+class UrdfCalibrator
+{
+public:
 
-    UrdfCalibrator(ros::NodeHandle nh);
-    ~UrdfCalibrator();
-    /**
-      * @brief: function to load all data of the joint to be calibrated in to the struct
-      * @param: joint: joint to which data is loaded
-      */
-    void getJointInitFromUrdf(JointToCalibrate &joint);
+  UrdfCalibrator(ros::NodeHandle nh);
+  ~UrdfCalibrator();
+  /**
+  * @brief: function to load all data of the joint to be calibrated in to the struct
+  * @param: joint: joint to which data is loaded
+  */
+  void getJointInitFromUrdf(JointToCalibrate &joint);
 
-    /**
-      * @brief: function to wrtie all data of the calibrated joint to urdf
-      * @param: joint: data of the joint which is written to urdf.
-      */
-    void writeJointsToUrdf(double* optimized_joint_values);
+  /**
+  * @brief: function to wrtie all data of the calibrated joint to urdf
+  * @param: joint: data of the joint which is written to urdf.
+  */
+  void writeJointsToUrdf(double* optimized_joint_values);
 
-    /**
-      * @brief: service server which will receive command to get data from tf and opti track and minimise overall error
-      * @param: req request for service. if 1 then data is added to optimization block. if 2 optimizer solves the problem.
-      * @return: always returns true.
-      */
-    bool calibrateUrdfSrvServer(walkman_urdf_calibration::OptimizeUrdfJoints::Request &req,walkman_urdf_calibration::OptimizeUrdfJoints::Response &res );
+  /**
+  * @brief: service server which will receive command to get data from tf and opti track and minimise overall error
+  * @param: req request for service. if 1 then data is added to optimization block. if 2 optimizer solves the problem.
+  * @return: always returns true.
+  */
+  bool calibrateUrdfSrvServer(walkman_urdf_calibration::OptimizeUrdfJoints::Request &req,walkman_urdf_calibration::OptimizeUrdfJoints::Response &res );
 
-    std::string urdf_file_name_; /**<urdf file name */
-    std::vector<std::string> joint_names_; /**< vector of names of joints to be calibrated */
-    std::vector<JointToCalibrate> joints_to_calibrate_; /**< vector of joints to be calibrated */
-    urdf::Model robot_urdf_model_; /**< variable to store the urdf model */
-    TiXmlDocument urdf_xml_doc_; /**< object to xml doc, to be used for modifcation */
-    //TiXmlElement robot_element_; /**< object to element in xml doc */
-    ceres::Problem* ceres_problem_; /**< pointer to ceres problem **/
-    ros::ServiceServer optimization_service_server_; /**< service server for recording and optimizing data at will */
-    tf::TransformListener transform_listener_; /**< listens to all joint transforms */
-    std::string waist_opti_frame_str_, hand_opti_frame_str_; /**<string frames for the bodies tracked by opti-track */
-    double* free_params_init_; /**<pointer to the free params for optimizer */
-  };
+  /**
+  * @brief: spinner for the class/Node
+  */
+  void spin();
+  std::string urdf_file_name_,urdf_file_save_name_; /**<urdf file name */
+  std::vector<std::string> joint_names_; /**< vector of names of joints to be calibrated */
+  std::vector<JointToCalibrate> joints_to_calibrate_; /**< vector of joints to be calibrated */
+  urdf::Model robot_urdf_model_; /**< variable to store the urdf model */
+  TiXmlDocument urdf_xml_doc_; /**< object to xml doc, to be used for modifcation */
+  //TiXmlElement robot_element_; /**< object to element in xml doc */
+  ceres::Problem* ceres_problem_; /**< pointer to ceres problem **/
+  ros::ServiceServer optimization_service_server_; /**< service server for recording and optimizing data at will */
+  tf::TransformListener transform_listener_; /**< listens to all joint transforms */
+  std::string waist_opti_frame_str_, hand_opti_frame_str_; /**<string frames for the bodies tracked by opti-track */
+  double* free_params_init_; /**<pointer to the free params for optimizer */
+  ceres::CostFunction * cost;
+};
 
-  #endif
+#endif
--- walkman_urdf_calibration/src/calibrate_urdf.cpp
+++ walkman_urdf_calibration/src/calibrate_urdf.cpp
@@ -12,7 +12,6 @@ Eigen::Matrix4f tfToHomogenousMatrix(tf::StampedTransform stamped_tf)
   Eigen::Matrix4f transform_matrix;
   transform_matrix.setZero();
   Eigen::Matrix3d temp_rotation;
-  temp_rotation.setZero();
   tf::matrixTFToEigen(stamped_tf.getBasis(),temp_rotation);
   transform_matrix.block<3,3>(0,0)= temp_rotation.cast<float>();
   transform_matrix(0,3)=stamped_tf.getOrigin().x();
@@ -24,6 +23,7 @@ Eigen::Matrix4f tfToHomogenousMatrix(tf::StampedTransform stamped_tf)
 UrdfCalibrator::UrdfCalibrator(ros::NodeHandle nh)
 {
   nh.getParam("/urdf_filename", urdf_file_name_); //@TODO: Add exception handling
+  nh.getParam("/file_save_name", urdf_file_save_name_);
   nh.getParam("/joint_names", joint_names_);
   nh.getParam("/waist_in_optitrack",waist_opti_frame_str_);
   nh.getParam("/hand_in_optitrack", hand_opti_frame_str_);
@@ -47,26 +47,26 @@ UrdfCalibrator::UrdfCalibrator(ros::NodeHandle nh)
     joints_to_calibrate_.push_back(joint_it);
     ROS_INFO(" \nGot data of %s joint", joint_it.joint_name_.c_str());
     Eigen::Vector3f euler_angles=joint_it.parent_child_transform_.block<3,3>(0,0).eulerAngles(0,1,2);
-    free_params_init_[(i+1)*6-6]=joint_it.parent_child_transform_(0,3);
-    free_params_init_[(i+1)*6-5]=joint_it.parent_child_transform_(1,3);
-    free_params_init_[(i+1)*6-4]=joint_it.parent_child_transform_(2,3);
-    free_params_init_[(i+1)*6-3]=euler_angles[0];
-    free_params_init_[(i+1)*6-2]=euler_angles[1];
-    free_params_init_[(i+1)*6-1]=euler_angles[2];
+    free_params_init_[(i+1)*6-6]=double(joint_it.parent_child_transform_(0,3));
+    free_params_init_[(i+1)*6-5]=double(joint_it.parent_child_transform_(1,3));
+    free_params_init_[(i+1)*6-4]=double(joint_it.parent_child_transform_(2,3));
+    free_params_init_[(i+1)*6-3]=double(euler_angles[0]);
+    free_params_init_[(i+1)*6-2]=double(euler_angles[1]);
+    free_params_init_[(i+1)*6-1]=double(euler_angles[2]);
     std::cout<<"\n"<<free_params_init_[i];
     i++;
   }
 
   ceres_problem_ = new ceres::Problem();
   optimization_service_server_= nh.advertiseService("/CalibrateUrdf_data", &UrdfCalibrator::calibrateUrdfSrvServer, this) ;
-
+  spin();
 }
 
 UrdfCalibrator::~UrdfCalibrator()
 {
-  urdf_xml_doc_.SaveFile();
+  urdf_xml_doc_.SaveFile(urdf_file_save_name_);
   if(ceres_problem_!=NULL)
-    delete ceres_problem_;
+  delete ceres_problem_;
   if(free_params_init_!=NULL)
   delete[] free_params_init_;
 
@@ -76,36 +76,68 @@ bool UrdfCalibrator::calibrateUrdfSrvServer(walkman_urdf_calibration::OptimizeUr
 {
   if(req.optimization_function == req.ADD_OPTIMIZATION_DATA)
   {
-    float joint_change_vector[joints_to_calibrate_.size()*6];
+    float* joint_change_vector;
+    joint_change_vector=new float[joint_names_.size()*6];
     tf::StampedTransform joint_transform;
-    Eigen::Matrix4f transform_matrix, change_in_transform_matrix;
+    Eigen::Matrix4f transform_matrix, change_in_transform_matrix,test_matrix;
     transform_matrix.setZero();
     change_in_transform_matrix.setZero();
     Eigen::Matrix3d temp_rotation;
     temp_rotation.setZero();
     int i=0;
+    test_matrix=Eigen::Matrix4f::Identity();
     for(std::vector<JointToCalibrate>::iterator it=joints_to_calibrate_.begin();it!=joints_to_calibrate_.end();++it)
     {
       transform_listener_.waitForTransform(it->parent_name_,it->child_name_,ros::Time(0), ros::Duration(6.0));
       transform_listener_.lookupTransform(it->parent_name_,it->child_name_,ros::Time(0),joint_transform );
-      tf::matrixTFToEigen(joint_transform.getBasis(),temp_rotation);
+      //tf::matrixTFToEigen(joint_transform.getBasis(),temp_rotation);
       transform_matrix=tfToHomogenousMatrix(joint_transform);
-      change_in_transform_matrix=transform_matrix.inverse()*it->parent_child_transform_;
-      Eigen::Vector3f euler_angles=change_in_transform_matrix.block<3,3>(0,0).eulerAngles(0,1,2);
+      change_in_transform_matrix=transform_matrix*it->parent_child_transform_.inverse();
+      test_matrix=(change_in_transform_matrix*it->parent_child_transform_)*test_matrix;
+      Eigen::Vector3f euler_angles=change_in_transform_matrix.block<3,3>(0,0).eulerAngles(0,1,2);\
+      std::cout<<"\n the goddamned change\n"<<change_in_transform_matrix<<"\n";
       joint_change_vector[(i+1)*6-6]=change_in_transform_matrix(0,3);
-      joint_change_vector[(i+1)*6-5]=change_in_transform_matrix(0,3);
-      joint_change_vector[(i+1)*6-4]=change_in_transform_matrix(0,3);
+      joint_change_vector[(i+1)*6-5]=change_in_transform_matrix(1,3);
+      joint_change_vector[(i+1)*6-4]=change_in_transform_matrix(2,3);
       joint_change_vector[(i+1)*6-3]=euler_angles[0];
       joint_change_vector[(i+1)*6-2]=euler_angles[1];
       joint_change_vector[(i+1)*6-1]=euler_angles[2];
       i++;
     }
+
     transform_listener_.waitForTransform(waist_opti_frame_str_,hand_opti_frame_str_,ros::Time(0), ros::Duration(6.0));
     transform_listener_.lookupTransform(waist_opti_frame_str_,hand_opti_frame_str_,ros::Time(0),joint_transform );
     transform_matrix=tfToHomogenousMatrix(joint_transform);
-    ceres::CostFunction * cost = MinimizeUrdfError::Create(transform_matrix,joint_change_vector,joints_to_calibrate_.size());
-    ceres_problem_->AddResidualBlock(cost,NULL,free_params_init_);
+    std::cout<<"\nestimated pose \n"<<test_matrix<<"\n actual pose\n"<<transform_matrix;
+    //cost = MinimizeUrdfError::Create(transform_matrix,joint_change_vector,joints_to_calibrate_.size());
+    /*double* residuals=new double[6];
+    double ** params = new double*[1];
+    params[0]=free_params_init_;
+
+    cost->Evaluate(params, residuals, NULL);*/
+
+    ceres_problem_->AddResidualBlock(
+      new ceres::NumericDiffCostFunction< MinimizeUrdfError,ceres::CENTRAL,6,60>(new MinimizeUrdfError(transform_matrix,joint_change_vector,joints_to_calibrate_.size())),NULL,free_params_init_);
+    //delete[] residuals;
+    ROS_INFO("\nAdded data");
   }
+  else
+  {
+    ceres::Solver::Options options;
+    options.use_nonmonotonic_steps = true;
+    options.function_tolerance = 1e-10;
+    options.parameter_tolerance = 1e-10;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.max_num_iterations = 1000;
+    options.minimizer_progress_to_stdout = true;
+    ceres::Solver::Summary summary;
+    ROS_INFO("Starting optimization");
+    ceres::Solve(options, ceres_problem_, &summary);
+    std::cout<<"\n"<<summary.FullReport();
+    writeJointsToUrdf(free_params_init_);
+    res.success=true;
+  }
+  return true;
 
 }
 
@@ -113,9 +145,9 @@ bool UrdfCalibrator::calibrateUrdfSrvServer(walkman_urdf_calibration::OptimizeUr
 void UrdfCalibrator::getJointInitFromUrdf(JointToCalibrate &joint)
 {
   joint.parent_child_transform_.setZero();
-  joint.parent_child_transform_(0,3)=float(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.x);
-  joint.parent_child_transform_(1,3)=float(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.y);
-  joint.parent_child_transform_(2,3)=float(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.z);
+  joint.parent_child_transform_(0,3)=double(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.x);
+  joint.parent_child_transform_(1,3)=double(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.y);
+  joint.parent_child_transform_(2,3)=double(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.position.z);
   joint.parent_child_transform_(3,3)=1.0;
   float w,x,y,z;
   w=float(robot_urdf_model_.getJoint(joint.joint_name_)->parent_to_joint_origin_transform.rotation.w);
@@ -145,6 +177,18 @@ void UrdfCalibrator::writeJointsToUrdf(double* optimized_joint_values)
       joint_xml->FirstChildElement("origin")->SetAttribute("xyz",ss_origin.str());
       joint_xml->FirstChildElement("origin")->SetAttribute("rpy",ss_rpy.str());
     }
+  }
+
+}
+
+void UrdfCalibrator::spin()
+{
+  ros::Rate spin_rate(1000);
+  int i=0;
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    spin_rate.sleep();
   }
 
 }
